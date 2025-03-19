a8934cab8ee5d72186dbeb82a0522832cb3e7c7a
Matheus Costa <matheus.design12@gmail.com>
2025-03-19 19:40:53 -0300

    feat(App): Setup initial modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,33 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { QuoteModule } from "./external/quote/quote.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { databaseConfig } from "./config/database.config";
+import { environmentConfig } from "./config/environment.config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, environmentConfig],
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get("database.host"),
+        port: configService.get("database.port"),
+        username: configService.get("database.username"),
+        password: configService.get("database.password"),
+        database: configService.get("database.name"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: configService.get("database.synchronize"),
+        logging: configService.get("database.logging"),
+      }),
+    }),
+    QuoteModule,
+  ],
 })
 export class AppModule {}
