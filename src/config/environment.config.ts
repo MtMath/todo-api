5326ecb7d96f7ce0d98ac1b252cb7c8fa6a13fb8
Matheus Costa <matheus.design12@gmail.com>
2025-03-20 00:29:39 -0300

    feat(Auth): Update env config

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -3,4 +3,8 @@ import { registerAs } from "@nestjs/config";
 export const environmentConfig = registerAs("environment", () => ({
   nodeEnv: process.env.NODE_ENV || "development",
   port: parseInt(process.env.PORT || "3000", 10),
+  jwt: {
+    secret: process.env.JWT_SECRET || "",
+    expiresIn: parseInt(process.env.JWT_EXPIRES_IN || "86400", 10),
+  },
 }));
