8faa27784da49c3f4502533c46e97710a466fb40
Matheus Costa <matheus.design12@gmail.com>
2025-03-20 00:44:31 -0300

    feat(Auth): Add user entitie and interface

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from "typeorm";
+import { Exclude } from "class-transformer";
+import { Task } from "@app/tasks/entities/task.entity";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column({ unique: true })
+  username!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  @Exclude({ toPlainOnly: true })
+  password!: string;
+
+  @Column({ default: true })
+  isActive!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @OneToMany(() => Task, (task) => task.user)
+  tasks!: Task[];
+}
